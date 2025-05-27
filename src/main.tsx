de3c8b2fb86696e8e91090a34a7f0e111c772501
Arpit Bansal <hi@arpitbansal.com>
2025-05-27 23:34:26 +0530

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <BrowserRouter basename="/shopify-watch">
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+);
